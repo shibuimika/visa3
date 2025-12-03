338cf2dee9c72797d191520b6c8764447a31f5be
shibuiimika <mika.shibui@gmail.com>
2025-12-04 00:20:45 +0900

    Update Next.js config for Netlify deployment

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, path: false };
+    return config;
+  },
 };
 
 export default withNextIntl(nextConfig);
